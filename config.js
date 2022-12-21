ab8cb4010fbb3d48de9fb76642a9ce8b32515ae8
chabaking <zaslake@gmail.com>
2022-12-21 13:06:25 +0100

    creando varibles de entorno en .env y configurandolas para exportacion en config

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    DISCORD_TOKEN : process.env.DISCORD_TOKEN,
+    CLIENT_ID : process.env.CLIENT_ID
+}
+
+//esto para tener tokens organizados y luego poder usar desde main
--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const {Client,GatewayIntentBits} = require("discord.js")
 
 const colors = require("colors")
 
-const token = "MTA1NTA1NTQ2Njk0Nzk0ODU3NA.Gl7qpx.RaHuWJoxNoxg_pC169no7s-Sk5_9qs8Ob4gRsE"
+//requerimos vvariables de entorno
+const {DISCORD_TOKEN,CLIENT_ID} = require("./config")
+
+const dotenv= require("dotenv")
+dotenv.config()
+
+// const token = "MTA1NTA1NTQ2Njk0Nzk0ODU3NA.Gl7qpx.RaHuWJoxNoxg_pC169no7s-Sk5_9qs8Ob4gRsE"
 
 const client = new Client( {
     intents: [
@@ -25,4 +31,4 @@ client.once("ready", ()=>{
     console.log(testchannel.find(channel => channel.name === "text"))
 })
 
-client.login(token)
+client.login(DISCORD_TOKEN)
